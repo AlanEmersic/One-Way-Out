863824005fc4a3cf018dfa3cb39b2e4dfa7831c0
Alan Emersic <alan.emersic@gmail.com>
2019-07-02 19:17:22 +0200

    Initital message

---
+++ Assets/Code/Maze/ColorGenerator.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ColorGenerator : MonoBehaviour
+{
+    enum Colour
+    {
+        Red, Blue, Green, Yellow, Orange, Purple
+    }
+
+    [SerializeField] GameObject spritePrefab;
+    [SerializeField] Colors colorsUI;
+    Queue<Colour> colorQueue;
+    Colour[] colors;
+
+    public void Initialize(int seed)
+    {
+        System.Random random = new System.Random(seed);
+        colors = Enum.GetValues(typeof(Colour)).Cast<Colour>().ToArray();
+        colorQueue = new Queue<Colour>();
+        int colorCount = random.Next(3, colors.Length);
+
+        foreach (Transform child in transform)
+            Destroy(child.gameObject);
+
+        for (int i = colors.Length - 1; i > 0; i--)
+        {
+            int j = random.Next(i + 1);
+            Colour tmp = colors[i];
+            colors[i] = colors[j];
+            colors[j] = tmp;
+        }
+
+        for (int i = 0; i < colorCount; i++)
+        {
+            colorQueue.Enqueue(colors[i]);
+            GameObject obj = Instantiate(spritePrefab, transform);
+            obj.GetComponent<Image>().sprite = GetColor(colors[i], obj);
+        }
+    }
+
+    Sprite GetColor(Colour color, GameObject obj)
+    {
+        for (int i = 0; i < colorsUI.sprites.Length; i++)
+        {
+            string name = colorsUI.sprites[i].name;
+            if (Enum.TryParse(name, out Colour colour) && colour == color)
+            {
+                obj.name = colorsUI.sprites[i].name;
+                return colorsUI.sprites[i];
+            }
+        }
+        return null;
+    }
+
+    public void ColorCompleted()
+    {
+        colorQueue.Dequeue();
+    }
+}
---
+++ Assets/Code/Maze/TaskGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskGenerator : MonoBehaviour
+{
+    public int TaskCount { get; private set; }
+    public List<Cell> TaskCells { get; private set; }
+
+    public void CreateTasks(Grid maze, int seed)
+    {
+        System.Random random = new System.Random(seed);
+        TaskCells = new List<Cell>();
+        List<Cell> deadEnds = maze.DeadEnds();
+
+        int min = Mathf.RoundToInt(maze.Rows * (1.0f / 3));
+        int max = Mathf.RoundToInt(maze.Rows * (2.0f / 3));
+        TaskCount = random.Next(min, max);
+        //print($"Tasks: {TaskCount}");
+        string taskName = "Tasks";
+
+        if (transform.Find(taskName))
+            DestroyImmediate(transform.Find(taskName).gameObject);
+
+        Transform tasksHolder = new GameObject(taskName).transform;
+        tasksHolder.parent = transform;
+
+        for (int i = 0; i < TaskCount;)
+        {
+            Cell cell = maze.RandomCell();
+            if (cell != maze.Start && cell != maze.End && deadEnds.Contains(cell) && !TaskCells.Contains(cell))
+            {
+                TaskCells.Add(cell);
+                //maze.CellTransform[cell].GetComponent<Renderer>().material.color = Color.blue;
+                i++;
+            }
+        }
+    }
+
+    public void OnTaskCompleted()
+    {
+        TaskCount--;
+        print($"Tasks left: {TaskCount}");
+    }
+}
